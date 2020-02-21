e5d473e1a3ca754faa8f23239fee7fc1a5bbe5c1
Ousmane! <ousmane.sylla@ynov.com>
2020-02-21 19:26:42 +0100

    mise en page footer v1

--- frontm1klns/src/layout/Footer.js
+++ frontm1klns/src/layout/Footer.js
@@ -1,11 +1,35 @@
 import React, {Component} from 'react';
+import '../assets/sass/layout/_Footer.scss';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import logo from '../assets/img/logo.svg';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {faFacebook, faTwitter, faLinkedin} from '@fortawesome/free-brands-svg-icons';
 
 class Footer extends Component {
     render() {
         return (
-            <div>
-                
+            <div className="footer">
+                <div className="row">
+                    <div className="col-md-3">
+                        <img src={logo} className="logo" alt="logo"/>
+                    </div>
+                    <div className="col-md-6">
+                        <p className="foot"> copyright 2020 </p>
+                    </div>
+                    <div className="col-md-3">
+                        <a href="#" className="facebook social">
+                            <FontAwesomeIcon icon={faFacebook} size="2x" />
+                        </a>
+                        <a href="#" className="twitter social">
+                            <FontAwesomeIcon icon={faTwitter} size="2x" />
+                        </a>
+                        <a href="#" className="linkedin social">
+                            <FontAwesomeIcon icon={faLinkedin} size="2x" />
+                        </a>
+                    </div>
+                </div>
             </div>
+
         );
     }
 }
--- frontm1klns/src/layout/Header.js
+++ frontm1klns/src/layout/Header.js
@@ -1,10 +1,12 @@
 import React, {Component} from 'react';
+import '../assets/sass/layout/_Header.scss';
+import image from "../assets/img/hero.png";
 
 class Header extends Component {
     render() {
         return (
             <div>
-                
+
             </div>
         );
     }
