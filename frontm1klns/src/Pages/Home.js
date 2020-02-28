e2c33cd411fbafd3afdfc752276f112768500855
Ousmane! <ousmane.sylla@ynov.com>
2020-02-28 19:49:59 +0100

    div avant le footer

--- frontm1klns/src/Pages/Home.js
+++ frontm1klns/src/Pages/Home.js
@@ -1,15 +1,41 @@
 import React, {Component} from 'react';
-import MainTitle from '../components/MainTitle';
+import '../assets/sass/base/_base.scss';
+import image from '../assets/img/bg-img.png';
+import 'bootstrap/dist/css/bootstrap.min.css';
+//import MainTitle from '../components/MainTitle';
 //import UsersList from '../components/UsersList';
+
 
 class Home extends Component {
     render() {
         return (
-            <div>
-                {/*<MainTitle text="WeDev"/>*/}
+            <div className="home-layout">
+                <section>
+                    <div className="row">
+                        <div className="col-md-6">
+                            <p>hnhj</p>
+                        </div>
+                        <div className="col-md-6">
+                            <p>jda</p>
+                        </div>
+                    </div>
+                </section>
+                <section>
+                    <div className="bg-img" style={{backgroundImage: `url(${image}` }}>
+                        <div className="box">
+                            <div className="content">
+                                <h1>offre de lancement</h1>
+                                <p>inscrivez-vous et recevez prochainement  <br/>un accès prenium à l'application WeAreData</p>
+                                <button className="button">
+                                    <span>je m'inscris</span>
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+                </section>
             </div>
+
         );
     }
 }
-
 export default Home;
--- frontm1klns/src/layout/Footer.js
+++ frontm1klns/src/layout/Footer.js
@@ -1,9 +1,9 @@
 import React, {Component} from 'react';
 import '../assets/sass/layout/_Footer.scss';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import logo from '../assets/img/logo.svg';
+import logo from '../assets/img/logo-footer.svg';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {faFacebook, faTwitter, faLinkedin} from '@fortawesome/free-brands-svg-icons';
+import {faFacebookF, faTwitter, faLinkedinIn} from '@fortawesome/free-brands-svg-icons';
 
 class Footer extends Component {
     render() {
@@ -18,13 +18,13 @@ class Footer extends Component {
                     </div>
                     <div className="col-md-3">
                         <a href="#" className="facebook social">
-                            <FontAwesomeIcon icon={faFacebook} size="2x" />
+                            <FontAwesomeIcon icon={faFacebookF} size="2x" />
                         </a>
                         <a href="#" className="twitter social">
                             <FontAwesomeIcon icon={faTwitter} size="2x" />
-                        </a>
+                    </a>
                         <a href="#" className="linkedin social">
-                            <FontAwesomeIcon icon={faLinkedin} size="2x" />
+                            <FontAwesomeIcon icon={faLinkedinIn} size="2x" />
                         </a>
                     </div>
                 </div>
